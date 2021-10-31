a9c2a4ac6cfd8125aedae93b3d23a91b674f3aeb
Shaun van Huyssteen <littleblueline@gmail.com>
2021-10-31 13:14:25 +0200

    added git ignore

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import handleRegister from "./controllers/register.js";
 import handleSignin from "./controllers/signin.js";
 import handleProfileGet from "./controllers/profile.js";
 import { handleImage, handleApiCall } from "./controllers/image.js";
+import gi from ('gitignore');
 
 const db = knex({
   client: "pg",
